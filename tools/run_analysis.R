401acdc630ec5e69cb6353af31fb7bb2dfea4fcf
zhouyiqi <zhouyiqi@singleronbio.com>
2020-07-03 13:29:11 +0800

    add mito.tsv

--- tools/run_analysis.R
+++ tools/run_analysis.R
@@ -16,12 +16,27 @@ sample = argv$sample
 matrix = read.table(matrix_file,sep="\t",header=TRUE,row.names=1)
 tsne.out = paste(outdir,"tsne_coord.tsv",sep="/")
 marker.out = paste(outdir,"markers.tsv",sep="/")
+mito.out = paste(outdir,"mito.tsv",sep="/")
 
 
 rds = CreateSeuratObject(raw.data = matrix,project=sample)
-mito.genes <- grep(pattern = "^MT-", x = rownames(x = rds@data), value = TRUE)
+
+# mito
+mito.genes <- grep(pattern = "^MT-", x = rownames(x = rds@data), value = TRUE, ignore.case=TRUE)
 percent.mito <- Matrix::colSums(rds@raw.data[mito.genes,])/Matrix::colSums(rds@raw.data)
 rds <- AddMetaData(object = rds, metadata = percent.mito, col.name = "percent.mito")
+meta = rds@meta.data
+total_cell = dim(meta)[1]
+percent_list = c(0.05,0.1,0.15,0.2)
+mito_df = dplyr::tibble(mito_percent=numeric(),cell_percent=numeric())
+for (percent in percent_list){
+  cell_percent = sum(meta$percent.mito > percent) / total_cell
+  mito_df = mito_df %>% dplyr::add_row(mito_percent=percent,cell_percent=cell_percent)
+}
+paste0(round(mito_df$cell_percent * 100,2),"%")
+mito_df$cell_percent = paste0(round(mito_df$cell_percent * 100,2),"%")
+mito_df$mito_percent = paste0(round(mito_df$mito_percent * 100,2),"%")
+write_tsv(mito_df,mito.out)
 
 rds <- NormalizeData(object = rds, normalization.method = "LogNormalize",scale.factor = 10000)
 rds <- FindVariableGenes(object = rds, mean.function = ExpMean, dispersion.function = LogVMR, x.low.cutoff = 0.1,  y.cutoff = 1)
