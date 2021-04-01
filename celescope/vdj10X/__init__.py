366c4444cc706e6672018332e03bb20bab3c5688
zhouyiqi <zhouyiqi@singleronbio.com>
2021-04-01 13:43:24 +0800

    ref update

--- celescope/vdj10X/__init__.py
+++ celescope/vdj10X/__init__.py
@@ -9,15 +9,26 @@ default_version = '4.0.0'
 species_choice = ['hs', 'mmu']
 soft_dict = {}
 ref_dict = {}
+
+def isDir(path_list):
+    for path in path_list:
+        if os.path.isdir(path):
+            return path
+
 for version in soft_version:
     soft_dict[version] = f'/SGRNJ/Database/script/soft/cellranger/cellranger-{version}/cellranger'
     ref_dict[version] = {}
     for species in species_choice:
+
         default_ref = glob.glob(
-            f'/SGRNJ/Database/script/soft/cellranger/vdj_ref/{default_version}/{species}/refdata*{default_version}')[0]
-        ref_path = glob.glob(f'/SGRNJ/Database/script/soft/cellranger/vdj_ref/{version}/{species}/refdata*{version}')
+            f'/SGRNJ/Database/script/soft/cellranger/vdj_ref/{default_version}/{species}/refdata*')
+        default_ref = isDir(default_ref)
+
+        ref_path = glob.glob(f'/SGRNJ/Database/script/soft/cellranger/vdj_ref/{version}/{species}/refdata*')
+        ref_path = isDir(ref_path)
+        
         if ref_path:
-            ref_dict[version][species] = ref_path[0]
+            ref_dict[version][species] = ref_path
         else:
             ref_dict[version][species] = default_ref
 
