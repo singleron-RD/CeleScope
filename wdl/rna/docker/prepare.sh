40acbad846af6c096c84d84b0ff9e996e2826d41
zhouyiqi <zhouyiqi@singleronbio.com>
2021-06-02 16:46:54 +0800

    update fq s3

--- wdl/rna/docker/prepare.sh
+++ wdl/rna/docker/prepare.sh
@@ -1,3 +1,4 @@
 git clone https://gitee.com/singleron-rd/celescope.git
 git clone https://gitee.com/singleron-rd/celescope_tests.git
 cd celescope/wdl/ && zip -r wdl.zip ./* && cd ../../
+cp ./celescope/wdl/rna/docker/submit.sh .
