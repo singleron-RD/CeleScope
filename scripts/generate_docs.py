f1cdbc9fa29e60b045ac4d5b0782870687b6fc34
zhouyiqi <zhouyiqi@singleronbio.com>
2021-06-28 11:23:16 +0800

    mv generate_doc.py to scripts

--- scripts/generate_docs.py
+++ scripts/generate_docs.py
@@ -1,15 +1,13 @@
 import argparse
 import inspect
 import os
-import importlib
-from collections import defaultdict
 
 import celescope.tools.utils as utils
 from celescope.celescope import ArgFormatter
-from celescope.__init__ import ASSAY_DICT, RELEASED_ASSAYS
+from celescope.__init__ import ASSAY_DICT, RELEASED_ASSAYS, ROOT_PATH
 
 PRE_PROCESSING_STEPS = ('sample', 'barcode', 'cutadapt')
-DOCS_DIR = 'docs/'
+DOCS_DIR = f'{ROOT_PATH}/../docs/'
 TEMPLATE_DIR = 'docs_template/'
 MANUAL = f'{DOCS_DIR}/manual.md'
 MANUAL_TEMPLATE = f'{DOCS_DIR}/manual_template.md'
@@ -120,40 +118,6 @@ class Docs():
         if not step in PRE_PROCESSING_STEPS:
             with open(MANUAL, 'a') as writer:
                 writer.write(f'- [{step}]({self.relative_md_path[step]})\n')
-    
-
-"""
-@utils.add_log
-def generate_all_docs():
-    md_path_dict = defaultdict(dict)
-
-    for assay in ASSAY_DICT:
-        init_module = utils.find_assay_init(assay)
-        steps = init_module.__STEPS__
-        generate_all_docs.logger.info(f"Writing docs {assay} ")
-
-        steps.append(f'multi_{assay}')
-        for step in steps:
-            generate_all_docs.logger.info(f"Writing doc {assay}.{step}")
-            md_path = generate_single_step_doc(assay, step)
-            md_path_dict[assay][step] = md_path
-    return md_path_dict
-"""
-
-
-@utils.add_log
-def write_manual(md_path_dict):
-    with open(MANUAL_MD, 'w') as manual_handle:
-        with open(MANUAL_TEMPLATE, 'r') as manual_template:
-            manual_handle.write(manual_template.read())
-        for assay in RELEASED_ASSAYS:
-            init_module = utils.find_assay_init(assay)
-            steps = init_module.__STEPS__
-            manual_handle.write(f'## {ASSAY_DICT[assay]}\n')
-            for step in steps:
-                write_manual.logger.info(f"Writing manual {assay}.{step}")
-                md_path = md_path_dict[assay][step]
-                write_step_in_manual(md_path, step, manual_handle)
 
 
 def main():
@@ -170,8 +134,6 @@ def main():
     for assay in ASSAY_DICT:
         docs_obj = Docs(assay)
         docs_obj.run()
-
-
 
 
 if __name__ == "__main__":
