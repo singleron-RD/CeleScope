4bc587956e0b6d04e2372fa3f3b6375ac2895f84
Tony Zhou <zhouyiqi@singleronbio.com>
2021-06-29 13:23:36 +0800

    Update analysis_mixin.py
    revert

--- celescope/tools/analysis_mixin.py
+++ celescope/tools/analysis_mixin.py
@@ -86,9 +86,12 @@ class AnalysisMixin():
         return html code
         """
 
+        avg_logfc_col = "avg_log2FC"  # seurat 4
+        if "avg_logFC" in self.marker_df.columns:  # seurat 2.3.4
+            avg_logfc_col = "avg_logFC"
         marker_df = self.marker_df.loc[:,
-            ["cluster", "gene", "avg_log2FC", "pct.1", "pct.2", "p_val_adj"]
-        ]
+                                       ["cluster", "gene", avg_logfc_col, "pct.1", "pct.2", "p_val_adj"]
+                                       ]
         marker_df["cluster"] = marker_df["cluster"].apply(lambda x: f"cluster {x}")
 
         return marker_df
