f62e77088fdbe5f0a157f6ffc31f085188dcc5e7
周鑫 <zhouxin@SGRNJshiny.sgr.com>
2021-09-06 11:17:44 +0800

    add index dir

--- celescope/trust_vdj/__init__.py
+++ celescope/trust_vdj/__init__.py
@@ -2,3 +2,4 @@ __STEPS__ = ['sample', 'barcode', 'cutadapt', 'matching', 'assemble']
 __ASSAY__ = 'trust_vdj'
 
 TRUST = '/SGRNJ03/randd/zhouxin/software/TRUST4/'
+INDEX = '/SGRNJ03/randd/zhouxin/data_base/index'
