fcd92a396cbad4b5e64374c815abc2b018b427c7
周鑫 <zhouxin@SGRNJshiny.sgr.com>
2021-09-13 18:03:49 +0800

    solve error

--- celescope/trust_vdj/__init__.py
+++ celescope/trust_vdj/__init__.py
@@ -1,8 +1,8 @@
 __STEPS__ = ['sample', 'barcode', 'cutadapt', 'assemble', 'summarize']
 __ASSAY__ = 'trust_vdj'
 
-TOOLS_DIR = '/SGRNJ03/randd/zhouxin/software/TRUST4/scripts'
-INDEX = '/SGRNJ03/randd/zhouxin/data_base/index'
+TOOLS_DIR = '/SGRNJ03/randd/cjj/zhouxin/zhouxin/software/TRUST4/scripts'
+INDEX = '/SGRNJ03/randd/cjj/zhouxin/zhouxin/data_base/index'
 CHAIN = {
 	'TCR': ['TRA', 'TRB'], 
 	'BCR': ['IGH', 'IGL', 'IGK']
@@ -11,4 +11,4 @@ PAIRED_CHAIN = {
 	'TCR': ['TRA_TRB'],
 	'BCR': ['IGH_IGL', 'IGH_IGK']
 }
-CONDA_PATH = '/SGRNJ/Public/Software/conda_env/zhouxinT'
+CONDA_PATH = '/SGRNJ/Public/Software/conda_env/vdj_trust4'
