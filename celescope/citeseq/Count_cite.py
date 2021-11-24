ed91babad88d9dca3a9f9c64f2fa484d075106bc
wuqi <muio123@qq.com>
2021-11-24 18:05:47 +0800

    refactor tag and vdj

--- celescope/citeseq/Count_cite.py
+++ celescope/citeseq/Count_cite.py
@@ -94,3 +94,24 @@ class Count_cite():
             stat_file=self.stat_file,
             outdir=self.outdir + '/..')
         t.get_report()
+
+def get_opts_count_cite(parser, sub_program):
+    parser.add_argument("--match_dir", help="matched scRNA-Seq CeleScope directory path", required=True)
+    if sub_program:
+        parser.add_argument('--outdir', help='output dir', required=True)
+        parser.add_argument('--sample', help='sample name', required=True)
+        parser.add_argument('--assay', help='assay', required=True)
+        parser.add_argument("--read_count_file", help="tag read count file")
+
+
+def count_cite(args):
+
+    count_cite_object = Count_cite(
+        args.sample,
+        args.outdir,
+        args.assay,
+        args.read_count_file,
+        args.match_dir,
+    )
+    count_cite_object.run()
+    count_cite_object.report()
