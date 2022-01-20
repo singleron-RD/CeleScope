8b88d196c3dba796c6c04e4308fc1569f823dec5
陈俊杰 <chenjunjie@SGRNJshiny.sgr.com>
2022-01-20 13:34:41 +0800

    relative path

--- celescope/trust_vdj/__init__.py
+++ celescope/trust_vdj/__init__.py
@@ -1,5 +1,11 @@
+import os
+import celescope.tools
+
 __STEPS__ = ['sample', 'barcode', 'cutadapt', 'assemble', 'summarize', 'mapping']
 __ASSAY__ = 'trust_vdj'
+
+TOOLS_DIR = os.path.dirname(celescope.tools.__file__) + '/trust4'
+INDEX = f'{TOOLS_DIR}/database'
 
 TOOLS_DIR = '/SGRNJ03/randd/cjj/zhouxin/zhouxin/software/TRUST4/scripts'
 INDEX = '/SGRNJ03/randd/cjj/zhouxin/zhouxin/data_base/index'
