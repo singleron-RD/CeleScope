d1bef45fc1b7c2339909ef91499df881d9831acd
陈俊杰 <chenjunjie@SGRNJshiny.sgr.com>
2022-01-20 13:40:49 +0800

    fix

--- celescope/trust_vdj/__init__.py
+++ celescope/trust_vdj/__init__.py
@@ -7,8 +7,6 @@ __ASSAY__ = 'trust_vdj'
 TOOLS_DIR = os.path.dirname(celescope.tools.__file__) + '/trust4'
 INDEX = f'{TOOLS_DIR}/database'
 
-TOOLS_DIR = '/SGRNJ03/randd/cjj/zhouxin/zhouxin/software/TRUST4/scripts'
-INDEX = '/SGRNJ03/randd/cjj/zhouxin/zhouxin/data_base/index'
 CHAIN = {
 	'TCR': ['TRA', 'TRB'], 
 	'BCR': ['IGH', 'IGL', 'IGK']
@@ -17,4 +15,3 @@ PAIRED_CHAIN = {
 	'TCR': ['TRA_TRB'],
 	'BCR': ['IGH_IGL', 'IGH_IGK']
 }
-CONDA_PATH = '/SGRNJ/Public/Software/conda_env/vdj_trust4'
