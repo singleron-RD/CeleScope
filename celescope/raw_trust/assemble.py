0b59cd38bfff0a5f9ec0fff0c826fb6c5b0fd6b3
陈俊杰 <chenjunjie@SGRNJlogin.sgr.com>
2022-03-30 15:32:42 +0800

    docs

--- celescope/raw_trust/assemble.py
+++ celescope/raw_trust/assemble.py
@@ -82,6 +82,26 @@ def get_full_len_assembly(outdir, sample):
 
 
 class Assemble(Step):
+
+    """
+    ## Features
+
+    - Assemble TCR/BCR seq data.
+
+    ## Output
+    - `03.assemble/{sample}_R1.fq` record barcode and umi info for assembly
+    - `03.assemble/{sample}_R1.fq` record sequence and quality info for assembly
+    - `03.assemble/{sample}_cdr3.out` All assembled CDR3 output and gene information.
+    - `03.assemble/{sample}_assign.out` read assignment results.
+    - `03.assemble/{sample}_assembled_reads.fa` Assembled raw reads.
+    - `03.assemble/{sample}_annot.fa` Assembled annotated contig sequences.
+    - `03.assemble/{sample}_full_len.fa` Assembled full length contig sequences.
+    - `03.assemble/{sample}_report.tsv` Record assembled CDR3 types, read count and proportion of read count.
+    - `03.assemble/{sample}_filter_report.tsv` Filter nonfunctional CDR3
+    - `03.assemble/{sample}_barcode_report.tsv` Record chain information for each barcode.
+    - `03.assemble/{sample}_barcode_filter_report.tsv` Filter low abundance cell.
+    """
+
     def __init__(self, args, display_title=None):
         Step.__init__(self, args, display_title=display_title)
         
--- celescope/raw_trust/summarize.py
+++ celescope/raw_trust/summarize.py
@@ -61,6 +61,20 @@ def gen_contig_csv(outdir, sample, full_len_assembly, assign_out):
 
 
 class Summarize(Step):
+
+    """
+    ## Features
+
+    - TCR/BCR full length assembly results.
+
+    ## Output
+    - `04.summarize/clonetypes.csv` High-level descriptions of each clonotype.
+    - `04.summarize/{sample}_all_contig.csv` High-level and detailed annotations of each contig.
+    - `04.summarize/{sample}_all_contig.fasta` All assembled contig sequences.
+    - `04.summarize/{sample}_filtered_contig.csv` High-level annotations of each cellular contig after filter. This is a subset of all_contig.csv.
+    - `04.summarize/{sample}_filtered_contig.fasta` Assembled contig sequences after filter.
+    """
+
     def __init__(self, args, display_title=None):
         Step.__init__(self, args, display_title=display_title)
 
