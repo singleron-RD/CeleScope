469eb3461ce7fadf46cd7173b1d60d451b199980
wuqi21 <muio123@qq.com>
2023-03-06 13:54:54 +0800

    add discrete to Tsne_dropdown_plot

--- celescope/tools/plotly_plot.py
+++ celescope/tools/plotly_plot.py
@@ -1,14 +1,13 @@
+import math
+import pathlib
 from collections import defaultdict
+import operator,functools
 
 import plotly
 import plotly.express as px
 import plotly.graph_objects as go
 
-import math
-import pathlib
-
 from celescope.tools import utils
-
 
 PLOTLY_CONFIG = {
     "displayModeBar": True,
@@ -175,12 +174,12 @@ class Tsne_plot(Plotly_plot):
 
 class Tsne_dropdown_plot(Plotly_plot):
 
-    def __init__(self, df_tsne,name, feature_name_list, dropdown=True):
+    def __init__(self, df_tsne,name, feature_name_list, discrete=False):
         super().__init__(df_tsne)
         
         self.name = name
         self.feature_name_list = feature_name_list
-        self.dropdown = dropdown
+        self.discrete = discrete
         self.title = f"t-SNE plot Colored by {self.name}"
         
         self._layout = {}
@@ -206,47 +205,110 @@ class Tsne_dropdown_plot(Plotly_plot):
         
     def get_plotly_div(self):
 
-        if self.dropdown:
-            self._fig = go.Figure()
-            _num = len(self.feature_name_list)
-            i=0 
-            
-            max_num = math.ceil(max([self._df[_].max() for _ in self.feature_name_list]))
-            split_num = math.ceil(max_num/5)
-            tick_list = list(range(0,max_num,split_num))
-
-            for feature_name in self.feature_name_list:
-                df_tmp =  self._df.loc[:,[self._str_coord1,self._str_coord2,feature_name]]
-                self._fig.add_trace(go.Scattergl(x=round_floats_in_list(df_tmp[self._str_coord1]),
-                                               y=round_floats_in_list(df_tmp[self._str_coord2]),
-                                               mode='markers',
-                                               name = feature_name[0].upper() + feature_name[1:],
-                                               showlegend=False,
-                                               marker=go.scattergl.Marker(opacity=0.9,size=3,color=self._df[feature_name],
-                                                                        cmax=max_num,
-                                                                        cmin=0,
-                                                                        colorscale=[[0,'LightGrey'],[1,'Red']],
-                                                                        colorbar=go.scattergl.marker.ColorBar(
-                                                                                                            tickmode='array',
-                                                                                                            tickvals= tick_list,
-                                                                                                            ticktext= tick_list,
-                                                                                                            title="",
-                                                                                                            titlefont={'size':11})),
-                                               textposition='top center'
-                                                    ))
-                visible_list=[False]*_num
-                visible_list[i]=True
-                i += 1
-                button = dict(args=[{"visible":visible_list},],
-                  label=feature_name,
-                  method="restyle",
-                 )
-                self._buttons.append(button)
+        if self.discrete:
+            self.discrete_tsne_plot()
         else:
-            pass
+            self.continuous_tsne_plot()
+            
         self.update_fig()
         
         return self.plotly_plot()
+        
+    def continuous_tsne_plot(self):
+        self._fig = go.Figure()
+        _num = len(self.feature_name_list)
+        i=0 
+
+        max_num = math.ceil(max([self._df[_].max() for _ in self.feature_name_list]))
+        split_num = math.ceil(max_num/5)
+        tick_list = list(range(0,max_num,split_num))
+
+        for feature_name in self.feature_name_list:
+            df_tmp =  self._df.loc[:,[self._str_coord1,self._str_coord2,feature_name]]
+            self._fig.add_trace(go.Scattergl(x=round_floats_in_list(df_tmp[self._str_coord1]),
+                                           y=round_floats_in_list(df_tmp[self._str_coord2]),
+                                           mode='markers',
+                                           name = feature_name[0].upper() + feature_name[1:],
+                                           showlegend=False,
+                                           marker=go.scattergl.Marker(opacity=0.9,size=3,color=self._df[feature_name],
+                                                                    cmax=max_num,
+                                                                    cmin=0,
+                                                                    colorscale=[[0,'LightGrey'],[1,'Red']],
+                                                                    colorbar=go.scattergl.marker.ColorBar(
+                                                                                                        tickmode='array',
+                                                                                                        tickvals= tick_list,
+                                                                                                        ticktext= tick_list,
+                                                                                                        title="",
+                                                                                                        titlefont={'size':11})),
+                                           textposition='top center'
+                                                ))
+            visible_list=[False]*_num
+            visible_list[i]=True
+            i += 1
+            button = dict(args=[{"visible":visible_list},],
+              label=feature_name,
+              method="restyle",
+             )
+            self._buttons.append(button)
+    
+    
+    def discrete_tsne_plot(self):    
+        all_figures =[]
+        num_all,last_num = 0,0
+        for feature_name in self.feature_name_list:
+            num_all += len(self._df[feature_name].unique())
+
+        for feature_name in self.feature_name_list:
+            df_sub = self._df.loc[:,['tSNE_1','tSNE_2',feature_name]]
+            num_feature = len(df_sub[feature_name].unique())
+
+            sum_df = df_sub.groupby(feature_name).agg("count").iloc[:, 0]
+            percent_df = sum_df.transform(lambda x: round(x / sum(x) * 100, 2))
+            res_dict = defaultdict(int)
+            res_list = []
+            for cluster in sorted(df_sub[feature_name].unique()):
+                name = f"{cluster}({percent_df[cluster]}%)"
+                res_dict[cluster] = name
+                res_list.append(name)
+
+            df_sub[feature_name] = df_sub[feature_name].map(res_dict)
+            df_sub['size'] = 4
+
+            scatter_config = {
+                    'data_frame': df_sub,
+                    'x': 'tSNE_1',
+                    'y': 'tSNE_2',
+                    'size_max':4,
+                    'hover_data': {
+                        'tSNE_1': False,
+                        'tSNE_2': False,
+                        feature_name: True,
+                        'size': False,
+                    },
+                    'size': 'size',
+                    'opacity': 0.9,
+                    'color': feature_name,
+                    'color_discrete_sequence': COLORS,
+                    'color_continuous_scale': px.colors.sequential.Jet,
+                }
+            fig = px.scatter( **scatter_config,
+                    category_orders={feature_name: res_list}
+                )
+            all_figures.append(fig)
+
+            true_list = [True]*num_feature
+            visible_list=[False]*num_all
+
+            visible_list[last_num:num_feature+last_num]=true_list
+            last_num += num_feature
+            button = dict(args=[{"visible":visible_list},],
+                          label=feature_name,
+                          method="update",
+            )
+            self._buttons.append(button)
+        
+        self._fig = go.Figure(data=functools.reduce(operator.add,[_.data for _ in all_figures]))
+        
         
     def update_fig(self):
         self._fig.update_xaxes(
