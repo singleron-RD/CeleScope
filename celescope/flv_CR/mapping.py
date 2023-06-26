088b291aa88186802e2e2998f253cf5badf8d858
Chenjunjie1996 <74704889+Chenjunjie1996@users.noreply.github.com>
2023-06-26 13:31:28 +0800

    flv (#246)
    * add split_fl_vdj in tag assay
    
    * add cr-count in convert10X
    
    * output filter_contig_annotation.csv
    
    * merge convert and add 10X chemistry option
    
    * celescope mkgtf
    
    * celescope mkgtf
    
    * lint
    
    * add utils assay
    
    * lint
    
    * lint
    
    * output fl metrics for each tag
    
    * update cr7.0 version
    
    * igblast vdj
    
    * refactor vdj
    
    * update docs
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * lint
    
    * conda install igblast, add mkref command
    
    * update docs
    
    * fix
    
    * lint
    
    * replace CDR3 with rearrangement's junction (CDR3)
    
    * add bcr docs
    
    * refactor analysis_snp
    
    * update analysis_snp docs
    
    * fix
    
    * fix igblast install
    
    * add bulk_vdj assay
    
    * fix docs
    
    * support customized pattern for flv
    
    * not print msg while rds not exists
    
    * fix
    
    * fix
    
    * do not filter output and fix protein name
    
    * fix
    
    * fix no exon numebr in snp
    
    * update convert10X docs
    
    * add split_fasta argument avoid running out of memory
    
    * update
    
    * output tsne-plot of assembled cells
    
    * add single heavy-light chain pair metric
    
    * lint
    
    * lint
    
    * gamma/delta tcr vdj ref
    
    * fix convert module
    
    * extract vdj mapping reads
    
    * lint
    
    * add refine_vdj step in flv_CR
    
    * lint
    
    * raise error when giving invalid filter method
    
    * fix merge
    
    * fix convert when barcodes exceed 737K whitelist
    
    * update
    
    * replace report with refine report
    
    * refactor gen_clonotypes_table function
    
    * lint
    
    * refine
    
    * fix when clonotype_id is Nan
    
    * add refine assemble step
    
    * fix plot_snp function
    
    * fix
    
    * remove R code in flv
    
    * fix
    
    ---------
    
    Co-authored-by: 陈俊杰 <chenjunjie@SGRNJlogin.sgr.com>
    Co-authored-by: 陈俊杰 <chenjunjie@SGRNJshiny.sgr.com>

--- celescope/flv_CR/mapping.py
+++ celescope/flv_CR/mapping.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import glob
-import subprocess
 import os
 import celescope
 import scanpy as sc
@@ -13,11 +12,6 @@ matplotlib.use('Agg')
 warnings.filterwarnings("ignore")
 
 TOOLS_DIR = os.path.dirname(celescope.tools.__file__)
-
-CELL_TYPE_DICT = {
-    'TCR':['T_cells', 'NKT_cells', 'T cells', 'NK T cells', 'Tcells'],
-    'BCR':['Plasma_cells', 'B_cells', 'Mature_B_cell', 'Plasma cells', 'B cells', 'Bcells'],
-    }
 
 
 class Mapping(Step):
@@ -33,60 +27,11 @@ class Mapping(Step):
     def __init__(self, args, display_title=None):
         Step.__init__(self, args, display_title=display_title)
         
-        self.seqtype = args.seqtype
         self.match_dir = args.match_dir
         if self.match_dir != 'None':
             self.contig_file = glob.glob(f'{args.match_out}/matched_contig_annotations.csv')[0]
-        self.celltype_set = CELL_TYPE_DICT[self.seqtype]
 
-        self.rds = glob.glob(f'{self.match_dir}/06.analysis/*.rds')
-        self.assign_file = glob.glob(f'{self.match_dir}/06.analysis/*_auto_assign/*_auto_cluster_type.tsv')
         self.h5ad = glob.glob(f'{self.match_dir}/06.analysis/*.h5ad')
-
-
-    @staticmethod
-    @utils.add_log
-    def run_mapping(rds, contig, sample, outdir, assign):
-        cmd = (
-            f'Rscript {TOOLS_DIR}/VDJmapping.R '
-            f'--rds {rds} '
-            f'--VDJ {contig} '
-            f'--sample {sample} '
-            f'--outdir {outdir} '
-            f'--assign_file {assign} '
-            '2>&1 '
-        )
-        subprocess.check_call(cmd, shell=True)
-
-    @utils.add_log
-    def process_rds(self):
-        """Mapping result with matched scRNA.
-        """
-        Mapping.run_mapping(
-            self.rds[0], self.contig_file, self.sample, self.outdir, self.assign_file[0]
-            )
-
-        meta = pd.read_csv(f'{self.outdir}/{self.sample}_meta.csv')
-        metaTB = meta[meta['CellTypes'].isin(self.celltype_set)]
-        mappedmeta = meta[meta['Class']=='T/BCR']
-        mappedmetaTB = mappedmeta[mappedmeta['CellTypes'].isin(self.celltype_set)]
-        
-        self.add_metric(
-            'Total Cell Number in Matched transcriptome',
-            meta.shape[0],
-            show=False)
-        self.add_metric(
-            'Cell Number Successfully Mapped to transcriptome',
-            mappedmeta.shape[0],
-            show=False)
-        self.add_metric(
-            'T/B cell Number in Matched transcriptome',
-            metaTB.shape[0],
-            show=False)
-        self.add_metric(
-            'Cell Number Successfully Mapped to T/B cell in transcriptome',
-            mappedmetaTB.shape[0],
-            show=False)
 
     @utils.add_log
     def process_h5ad(self):
@@ -103,8 +48,6 @@ class Mapping(Step):
         plt.savefig(f"{self.outdir}/{self.sample}_mapping.pdf", dpi=300)
 
     def run(self):
-        if self.rds and self.assign_file:
-            self.process_rds()
         if self.h5ad:
             self.process_h5ad()
 
@@ -116,7 +59,6 @@ def mapping(args):
 
 
 def get_opts_mapping(parser, sub_program):
-    parser.add_argument('--seqtype', help='TCR or BCR', choices=['TCR', 'BCR'], required=True)
     if sub_program:
         s_common(parser)
         parser.add_argument('--match_dir', help='scRNA-seq match directory', required=True)
