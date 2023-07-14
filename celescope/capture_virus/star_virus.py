a3f5f045613ba0f80edcfa5eb18a75817d7fde5a
zhouyiqi91 <386346852@qq.com>
2023-07-14 15:15:04 +0800

    add star_virus

---
+++ celescope/capture_virus/star_virus.py
@@ -0,0 +1,31 @@
+from celescope.tools import utils
+from celescope.tools.star_mixin import Star_mixin, get_opts_star_mixin
+from celescope.capture_virus.mkref import Mkref_virus
+
+
+class Star_virus(Star_mixin):
+    """
+    ## Features
+    - Map reads to the viral genome using STAR.
+
+    ## Output
+    - `{sample}_virus_Aligned.sortedByCoord.out.bam` : Aligned BAM sorted by coordinate.
+    """
+
+    def __init__(self, args, display_title=None):
+        # before init
+        args.genomeDir = args.virus_genomeDir
+
+        super().__init__(args, add_prefix='virus', display_title=display_title)
+        self.genome = Mkref_virus.parse_genomeDir(args.genomeDir)
+
+
+@utils.add_log
+def star_virus(args):
+    with Star_virus(args, display_title='Mapping') as runner:
+        runner.run()
+
+
+def get_opts_star_virus(parser, sub_program):
+    get_opts_star_mixin(parser, sub_program)
+    parser.add_argument('--virus_genomeDir', help='Virus genome directory.', required=True)
