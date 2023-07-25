c050c02016d3f4d107b0fef43c07a952bd7d7319
raulee <44012784+pigraul@users.noreply.github.com>
2023-07-25 12:19:44 +0800

    update dynaseq assay (#252)
    * update dynaseq assay
    
    Auto detect background snps for self control
    No strandness file required
    Multi processing for conversion and replacement
    Merge replacement and replace_tsne steps
    Generate h5ad and Matrix Market files for labeled and unlabeled transcripts
    Update plots and fix substitution plot bug
    Update html report
    Delete top TOR genes in clusters
    
    * fix bugs
    
    fix bugs according to setup_and lint
    
    * fix matrix bug
    
    fix matrix bug in replacement

--- celescope/tools/matrix.py
+++ celescope/tools/matrix.py
@@ -1,6 +1,7 @@
 import scipy.io
 import scipy.sparse
 import pandas as pd
+import numpy as np
 
 from celescope.tools.__init__ import (BARCODE_FILE_NAME, FEATURE_FILE_NAME, MATRIX_FILE_NAME)
 from celescope.tools import utils
@@ -157,3 +158,29 @@ class CountMatrix:
     def get_matrix(self):
         return self.__matrix
 
+    @classmethod
+    def dataframe_to_matrix(cls, df, features: Features, barcodes=None, barcode_column='Barcode', feature_column='geneID'):
+        """Convert a counts dataframe to a sparse counts matrix.
+        :param df_counts: counts dataframe
+        :type df_counts: pandas.DataFrame
+        :param barcodes: list of barcodes that will map to the rows
+        :type barcodes: list
+        :param features: list of features that will map to the columns
+        :type features: list
+        :param barcode_column: column in counts dataframe to use as barcodes, defaults to `Barcode`
+        :type barcode_column: str
+        :param feature_column: column in counts dataframe to use as features, defaults to `geneID`
+        :type feature_column: str
+
+        :return: sparse counts matrix
+        :rtype: scipy.sparse.csrmatrix
+        """
+        barcode_indices = {barcode: i for i, barcode in enumerate(barcodes)}
+        feature_indices = {feature: i for i, feature in enumerate(features.gene_id)}
+
+        matrix = scipy.sparse.lil_matrix((len(barcodes), len(features.gene_id)), dtype=np.float32)
+        for (barcode, feature), count in df.groupby([barcode_column, feature_column], sort=False,
+                                                        observed=True).size().items():
+            matrix[barcode_indices[barcode], feature_indices[feature]] = count
+
+        return matrix.tocsr()
