1db1d5b429b4f78785a0521e4b8081f3e577a3e9
zhouyiqi91 <386346852@qq.com>
2023-11-06 13:42:39 +0800

    add comment_metric_list in merge.xls

--- celescope/tools/merge_table.py
+++ celescope/tools/merge_table.py
@@ -17,7 +17,7 @@ def append_sample_data(sample, sample_data_dict, all_data_dict, steps):
     for step in steps:
         title = step + '_summary'
         if title in sample_data_dict:
-            metric_list = sample_data_dict[title]['metric_list']
+            metric_list = sample_data_dict[title]['metric_list'] + sample_data_dict[title]['comment_metric_list']
             if metric_list:
                 metric_dict = {'sample': sample}
                 for metric in metric_list:
